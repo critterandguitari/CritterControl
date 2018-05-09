9439c18293e57e97fa93d874b6a9cce34b5c3dd4
Owen Osborn <owen@critterandguitari.com>
2018-05-09 02:58:48 +0000

    better patc list

--- apps/PatchLauncher/static/app.js
+++ apps/PatchLauncher/static/app.js
@@ -1,6 +1,6 @@
 
 var appBaseURL = '/patchselect'
-
+var currentPatch = ''
 
 $(document).ajaxStart(function(){
 	$.LoadingOverlay("show", {
@@ -12,6 +12,23 @@ $(document).ajaxStart(function(){
 $(document).ajaxStop(function(){
     $.LoadingOverlay("hide");
 });
+
+function getCurrentPatch() {
+    $.get(appBaseURL+'/current')
+    .done(function (data) {
+        $('#current-patch').empty();
+        $('#current-patch').append('<p>'+data+'</p>');
+        currentPatch = data;
+        $('button').each(function() {
+            $(this).removeClass('btn-success').addClass('btn-primary');
+            if ($(this).data("patch") == currentPatch) $(this).removeClass('btn-primary').addClass('btn-success');
+        });
+       
+    })
+    .fail(function () {
+        console.log('problem');
+    });
+}
 
 $(function () {
     /*$.get(appBaseURL+'/info')
@@ -25,23 +42,15 @@ $(function () {
     .fail(function () {
         console.log('problem');
     });*/
-
-    $.get(appBaseURL+'/current')
-    .done(function (data) {
-        $('#current-patch').empty();
-        $('#current-patch').append('<p>'+data+'</p>');
-    })
-    .fail(function () {
-        console.log('problem');
-    });
-
     $.get(appBaseURL+'/list_patches')
     .done(function (data) {
         data.forEach(function(p) {
-            selectable_patch = $('<p class="selectable-patch">'+p+'</p>');
+            //selectable_patch = $('<p class="selectable-patch">'+p+'</p>');
+            selectable_patch = $('<button type="button" class="selectable-patch btn btn-primary">'+p+'</button>');
             selectable_patch.data("patch", p);  
-            $('#patch-list').append(selectable_patch);
+            $('#patch-list').append('<p></p>').append(selectable_patch);
         });
+        getCurrentPatch();
     })
     .fail(function () {
         console.log('problem');
@@ -56,6 +65,7 @@ $(function () {
             console.log('started: ' + data);
             $('#current-patch').empty();
             $('#current-patch').append('<p>'+data+'</p>');
+            getCurrentPatch();
         })
         .fail(function () {
             console.log('problem');
